71c0717e3384d478266fd943e7b98a925f684395
gitatractivo <gitanshutalwar@gmail.com>
2024-03-10 19:10:07 +0530

    email

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,6 +1,7 @@
 export const emails: string[] = [
   "shivangbirla9999@gmail.com",
   "gitanshutalwarrandom@gmail.com",
+  "gitanshutalwar@gmail.com",
   "Vipul@bigwigmedia.in",
   "aditya.chat@gmail.com",
   "devashish@bigwigmedia.in",
